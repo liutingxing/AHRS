128fa42ab96d6a56c6d431c41985c34ea7237d72
Anakin <jiangtianyu007@sina.com>
2017-09-30 15:41:33 +0800

    Add plot script for java simulation platform.
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

---
+++ ahrs_platform/ahrs_java_pcsim/data/dataPlot.m
@@ -0,0 +1,63 @@
+clear all;
+clc;
+
+addpath('..\..\..\');
+
+fin = fopen('outputData.txt', 'r');
+if fin == -1
+    fprintf('file not exist');
+    exit(-1);
+end
+
+while ~feof(fin)
+    line = fgets(fin);
+
+    entries = regexp(line, ' ', 'split');
+    
+    time = char(entries(1));
+    q0 = char(entries(5));
+    q1 = char(entries(6));
+    q2 = char(entries(7));
+    q3 = char(entries(8));
+    
+    time = str2num(time);
+    q0 = str2num(q0);
+    q1 = str2num(q1);
+    q2 = str2num(q2);
+    q3 = str2num(q3);
+    
+    % convert the left hand frame to right hand frame
+    q1 = -q1;
+    q2 = -q2;
+    
+    % convert quarterion to euler
+    q = [q0, q1, q2, q3];
+    Cbn = q2dcm(q);
+    [yaw(time), pitch(time), roll(time)] = dcm2euler(Cbn); 
+end
+
+% yaw
+figure;
+plot(yaw*180/pi, 'r');
+grid on;
+title('yaw');
+xlabel('sample point');
+ylabel('yaw (degree)');
+
+% pitch
+figure;
+plot(pitch*180/pi, 'r');
+grid on;
+title('pitch');
+xlabel('sample point');
+ylabel('pitch (degree)');
+
+% roll
+figure;
+plot(roll*180/pi, 'r');
+grid on;
+title('roll');
+xlabel('sample point');
+ylabel('roll (degree)');
+
+fclose(fin);
