eb19b291469276162eecff6c1ef6b90207091c3f
Anakin <jiangtianyu007@sina.com>
2017-11-02 10:45:45 +0800

    [BUG] Add sample data class for SHA-1: 771cba8e4029260e6d6ded022caa9eac8841b761 commit.
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

---
+++ ahrs_platform/ahrs_java_pcsim/src/bottomnav/hitherejoe/com/bottomnavigationsample/SampleData.java
@@ -0,0 +1,26 @@
+package bottomnav.hitherejoe.com.bottomnavigationsample;
+
+public class SampleData {
+    public int          uTime;
+    public double       fPsiPl;
+    public double       fThePl;
+    public double       fPhiPl;
+    public double[][]   fCnb = new double[3][3];
+    public double[][]   fCbn = new double[3][3];
+    public double[]     fqPl;
+    public double       fLinerAccN;
+    public double       fLinerAccE;
+    public double       fLinerAccD;
+    public double       fVelN;
+    public double       fVelE;
+    public double       fVelD;
+    public double       fPosN;
+    public double       fPosE;
+    public double       fPosD;
+    public double[]     fOmegaB;
+    public double[]     fAccelerate;
+    public double[]     fMagnetic;
+    public double       fAudio;
+    public double       fVel;
+    public double[]     fOmegaN = new double[3];
+}
