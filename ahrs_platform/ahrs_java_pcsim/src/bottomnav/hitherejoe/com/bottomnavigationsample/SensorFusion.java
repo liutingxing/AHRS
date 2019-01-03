ef65fcb3f70461786668ce9fe5a2da4b161b700e
Anakin <jiangtianyu007@sina.com>
2019-01-03 16:32:20 +0800

    [JAVA] Remove the mag invalid check in alignment process.
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

--- ahrs_platform/ahrs_java_pcsim/src/bottomnav/hitherejoe/com/bottomnavigationsample/SensorFusion.java
+++ ahrs_platform/ahrs_java_pcsim/src/bottomnav/hitherejoe/com/bottomnavigationsample/SensorFusion.java
@@ -1242,27 +1242,16 @@ public class SensorFusion {
 
     private int staticDetectUpdate(double[] gyro, double[] acc, double[] mag)
     {
-        if (fAlignGyroArray.size() < ALIGN_NUM)
-        {
-            fAlignAccArray.add(new double[]{acc[0], acc[1], acc[2]});
-            fAlignGyroArray.add(new double[]{gyro[0], gyro[1], gyro[2]});
-            if (MAG_SUPPORT == 1  && mag[0] != 0 && mag[1] != 0 && mag[2] != 0)
-            {
-                fAlignMagArray.add(new double[]{mag[0],mag[1],mag[2]});
-            }
-        }
-        else
+        if (fAlignGyroArray.size() >= ALIGN_NUM)
         {
             fAlignAccArray.remove(0);
             fAlignGyroArray.remove(0);
-            fAlignAccArray.add(new double[]{acc[0], acc[1], acc[2]});
-            fAlignGyroArray.add(new double[]{gyro[0], gyro[1], gyro[2]});
-            if (MAG_SUPPORT == 1  && mag[0] != 0 && mag[1] != 0 && mag[2] != 0)
-            {
-                fAlignMagArray.remove(0);
-                fAlignMagArray.add(new double[]{mag[0],mag[1],mag[2]});
-            }
+            fAlignMagArray.remove(0);
         }
+
+        fAlignAccArray.add(new double[]{acc[0], acc[1], acc[2]});
+        fAlignGyroArray.add(new double[]{gyro[0], gyro[1], gyro[2]});
+        fAlignMagArray.add(new double[]{mag[0],mag[1],mag[2]});
 
         return 0;
     }
