c181f3d22ac8d4ada8821819b149e0af25dd0b3b
Anakin <jiangtianyu007@sina.com>
2019-01-14 16:39:57 +0800

    [MATLAB] Update the interpolation script.
    - source data for the intepolation must be the raw gyro data, rather than the filtered data.
    
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

--- data/interpolation.m
+++ data/interpolation.m
@@ -9,48 +9,63 @@ Acc = sensor_data(:, 5:7);                     % ( m/s2 )
 Mag = sensor_data(:, 8:10);                    % ( uT )
 
 %% constant value
-cut_off_value = 900;
+cut_off_value = 1100;
 sample_num = 2;
 
-%% display filtered gyroZ
-
-% 5Hz low pass filter. 100Hz sample rate
-[b, a] = butter(2, 4/(100/2), 'low');
-GyroFiltered = filter(b, a, Gyro(:, 3));
+%% display real gyroZ
+GyroZ = Gyro(:,3)*180/pi;
 figure
-plot(GyroFiltered*180/pi, 'b');
-title('gyro z filtered data');
+plot(GyroZ, 'b');
+title('gyro z real data');
 hold on;
 xlim = get(gca,'Xlim');
 plot(xlim, [cut_off_value, cut_off_value], 'g');
 
 %% spline simulation
 
-curve = GyroFiltered*180/pi;
+curve = GyroZ;
 [value, index] = max(curve);
 
 % determin the sample point
 for i = index:-1:1
     if (curve(i) < cut_off_value)
+        x_left_index = i;
         break
     end
 end
-x_left = i-sample_num:i;
+x_left = x_left_index-sample_num:x_left_index;
 
 for i = index : length(curve)
     if (curve(i) < cut_off_value)
+        x_right_index = i;
         break
     end
 end
-x_right = i:i+sample_num;
+x_right = x_right_index:x_right_index+sample_num;
 
 % interpolation
 x = [x_left x_right];
 y = curve(x);
-xx = x_left+1:x_right-1;
+xx = x_left_index:x_right_index;
 yy = spline(x,y,xx);
 plot(x,y,'r o',xx,yy,'r');
 legend('real data', 'cut off', 'sample data', 'spline data');
+
+%% filter the real data and interpolation data
+[b, a] = butter(2, 4/(100/2), 'low');
+GyroFiltered = filter(b, a, GyroZ);
+GyroInterpolation = [GyroZ(1:x_left_index-1)', yy, GyroZ(x_right_index+1:length(GyroZ))'];
+GyroInterpolationFiltered = filter(b, a, GyroInterpolation);
+
+figure;
+plot(GyroFiltered, 'r');
+title('data comparison');
+hold on;
+plot(GyroInterpolationFiltered, 'b');
+legend('real data', 'spline data')
+
+
+
 
 
 
