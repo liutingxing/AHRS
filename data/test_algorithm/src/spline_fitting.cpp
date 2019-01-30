ad51bca8b400868beaf400062ba489b095200b9f
Anakin <jiangtianyu007@sina.com>
2019-01-30 15:39:47 +0800

    [C++] Create the spine fitting algorithm test project.
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

---
+++ data/test_algorithm/src/spline_fitting.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jiangtianyu on 2019/1/30.
+//
+#include <iostream>
+#include <Eigen/Core>
+#include <unsupported/Eigen/Splines>
+
+
+int main(int argc, char const* argv[])
+{
+    std::cout << "hello world!" << std::endl;
+}
