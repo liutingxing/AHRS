48c6d7c92f861b73e618467374c058a12b90484c
Anakin <jiangtianyu007@sina.com>
2019-02-21 11:13:21 +0800

    [MATLAB] Optimize the display quality.
    Signed-off-by: Anakin <jiangtianyu007@sina.com>

--- ahrs_platform/ahrs_java_pcsim/data/compensateDisplay.m
+++ ahrs_platform/ahrs_java_pcsim/data/compensateDisplay.m
@@ -9,28 +9,28 @@ GyroRaw = sensor_data_Raw(:, 2:4)*180/pi;          % ( degree/s )
 GyroFine = sensor_data_Fine(:, 2:4)*180/pi;        % ( degree/s )
 
 figure;
-plot(GyroRaw(:, 1), 'r', 'LineWidth',2);
-hold on;
 plot(GyroFine(:, 1), ':c');
+hold on;
+plot(GyroRaw(:, 1), 'r', 'LineWidth',2);
 title('gyro x compensate');
-legend('raw', 'fine');
+legend('fine', 'raw');
 xlabel('sample point');
 ylabel('gyro (m/s2)');
 
 figure;
-plot(GyroRaw(:, 2), 'r');
-hold on;
 plot(GyroFine(:, 2), ':c');
+hold on;
+plot(GyroRaw(:, 2), 'r');
 title('gyro y compensate');
-legend('raw', 'fine');
+legend('fine', 'raw');
 xlabel('sample point');
 ylabel('gyro (m/s2)');
 
 figure;
-plot(GyroRaw(:, 3), 'r');
-hold on;
 plot(GyroFine(:, 3), ':c');
+hold on;
+plot(GyroRaw(:, 3), 'r');
 title('gyro z compensate');
-legend('raw', 'fine');
+legend('fine', 'raw');
 xlabel('sample point');
 ylabel('gyro (m/s2)');
